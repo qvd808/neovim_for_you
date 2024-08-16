4116f70d96e0955539b4d3f94192ba4de2e4d042
Quang Vinh Dang <dqvinh101@gmail.com>
2024-08-16 16:25:09 -0700

    Adding practice for horizontal movements

---
+++ horizontal_movement.js
@@ -0,0 +1,6 @@
+function main() {
+	console.log("Some roses are red")
+	console.log("Violets some, are blue")
+	console.log("Programming is funny")
+	console.log("And some are you")
+}
---
+++ horizontal_movement_final.js
@@ -0,0 +1,6 @@
+function main() {
+	console.log("roses are red")
+	console.log("Violets are blue")
+	console.log("Programming is fun")
+	console.log("And so are you")
+}
